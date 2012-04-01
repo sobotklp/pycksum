e56be7b329fb1f4ddd60819959ce3c9f340dab65
Lewis <lewis@semadic.com>
2012-04-01 13:00:20 -0700

    test.py now takes multiple input files (same as cksum)

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import sys, cksum
 
 def main():
-    fname = sys.argv[-1]
-    fd = open(fname, "rb")
+    fnames = sys.argv[1:]
+    for fname in fnames:
+        fd = open(fname, "rb")
 
-    c = cksum.Cksum()
-    _sz = 0
-    while 1:
-        data = fd.read(4096)
-	if len(data) == 0:
-	    break
-	c.add(data)
-	_sz += len(data)
-    print "%d\t%d\t%s" %  (c.get_cksum(), _sz, fname)
+        c = cksum.Cksum()
+        sz = 0
+        while 1:
+            data = fd.read(4096)
+            if len(data) == 0:
+	        break
+            c.add(data)
+            sz += len(data)
+
+        print "%d\t%d\t%s" %  (c.get_cksum(), sz, fname)
 
 if __name__ == "__main__":
     main()
