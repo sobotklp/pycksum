7a3ca8bd8c6aca97e3b073ae717f2cb8366666db
Lewis <lewis@semadic.com>
2012-04-13 21:41:27 -0700

    Can run setup.py without Cython now

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-version_info = [0,9,0]
-version = ".".join(map(str, version_info))
+from pycksum import version
+
+try:
+    from Cython.Distutils import build_ext
+    cmdclass = {'build_ext': build_ext}
+    ext_modules = [
+    	Extension("_pycksum", ["ext/_pycksum.pyx"])
+    ]
+except ImportError:
+    cmdclass = {}
+    ext_modules = [
+    	Extension("_pycksum", ["ext/_pycksum.c"])
+    ],
 
 setup(
     name = "cksum",
@@ -11,9 +21,6 @@ setup(
     version = version,
     author = 'Lewis Sobotkiewicz',
     author_email = 'lewis.sobot@gmail.com',
-
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [
-    	Extension("_cksum", ["ext/_cksum.c"])
-    ],
+    cmdclass = cmdclass,
+    ext_modules = ext_modules,
 )
