2e701a8b783f4cec4622d34ea875506b22e5838c
Lewis <lewis@semadic.com>
2012-04-15 10:23:16 -0700

    Separate cython extension build out of setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,8 @@
+from setuptools import setup
+from setuptools.extension import Extension
 from pycksum import version
 
 long_description = open('README', 'rb').read()
-
-# Workaround for Cython's build_ext not working with setuptools
-try:
-    from Cython.Distutils import build_ext
-    from distutils.core import setup
-    from distutils.extension import Extension
-    cmdclass = {'build_ext': build_ext}
-    ext_modules = [
-    	Extension("_pycksum", ["ext/_pycksum.pyx"]),
-    ]
-except ImportError:
-    from setuptools import setup, find_packages
-    from setuptools.extension import Extension
-    cmdclass = {}
-    ext_modules = [
-    	Extension("_pycksum", ["ext/_pycksum.c"])
-    ]
 
 setup(
     name = "pycksum",
@@ -28,8 +13,9 @@ setup(
     version = version,
     author = 'Lewis Sobotkiewicz',
     author_email = 'lewis.sobot@gmail.com',
-    cmdclass = cmdclass,
-    ext_modules = ext_modules,
+    ext_modules = [
+        Extension("_pycksum", ["ext/_pycksum.c"])
+    ],
     entry_points = {
         'console_scripts' : [
             'pycksum = pycksum.main:main',
